d19c849bca50c6c3aa15ea75076f2a8c738b0128
RaviSaini045 <103064016+RaviSaini045@users.noreply.github.com>
2024-02-28 15:35:44 +0530

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,50 @@
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const BG_IMG="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const USER_AVATAR="https://okneeraj.github.io/netflix-gpt//avatar-red.jpeg";
+
+export const API_KEY = "";
+
+export const API_READ_ACCESS_TOKEN =
+  "";
+
+export const OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer Token",
+  },
+};
+
+export const GET_MOVIES_API="https://api.themoviedb.org/3/movie/";
+
+export const SEARCH_MOVIE_BY_NAME = "https://api.themoviedb.org/3/search/movie?query=";
+
+export const VIDEO_DATA_URL_1 ="https://api.themoviedb.org/3/movie/";
+
+
+export const VIDEO_DATA_URL_2 = "/videos?language=en-US";
+
+export const YOUTUBE_VIDEO_URL = "https://www.youtube.com/embed/";
+
+export const IMG_CDN_URL="https://image.tmdb.org/t/p/w500/";
+
+export const OPENAI_KEY = "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";
+
+export const SUPPORTED_LANGUAGE=[
+  {
+    identifier: "en",
+    name: "English",
+  },
+  {
+    identifier: "hindi",
+    name: "Hindi",
+  },
+  {
+    identifier: "spainish",
+    name: "Spainish",
+  },
+]
